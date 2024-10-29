80ff8b4c1632d51c302280f9b857d911982915c9
helenaskagerlid <helenaskagerlid@gmail.com>
2024-10-29 17:06:31 +0100

    add usertext to saveCriticalThoughts function

--- yourinnercritic/src/components/CriticThoughtsComponent.tsx
+++ yourinnercritic/src/components/CriticThoughtsComponent.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from "react";
 import { saveCriticalThoughts } from "../helpers/saveToLocalStorage";
 
 interface IPropsCriticThoughtsComponent {
@@ -7,21 +8,29 @@ interface IPropsCriticThoughtsComponent {
 export const CriticThoughtsComponent = ({
   criticThoughtsRef,
 }: IPropsCriticThoughtsComponent) => {
+  const [userInput, setUserInput] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    saveCriticalThoughts(userInput);
+    setUserInput("");
+  };
   return (
     <>
       <section ref={criticThoughtsRef}>
         <h2>Step 1: Your inner Ciritic's Thoughts</h2>
         <p>
           Take a moment to notice which kinds of thoughts your inner ciritc is
-          thinking in this situation. Then write them down in here below:
+          thinking in this situation. Then write them down here below:
         </p>
-        <button
-          onClick={() => {
-            saveCriticalThoughts("Testing");
-          }}
-        >
-          Save
-        </button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <button>Save</button>
+        </form>
       </section>
     </>
   );
