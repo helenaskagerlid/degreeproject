c3a3804ddd9b408865d13d01b7a36f282e387472
helenaskagerlid <helenaskagerlid@gmail.com>
2024-11-13 16:50:51 +0100

    :sparkles: display ClosingComponent

--- yourinnercritic/src/components/ClosingComponent.tsx
+++ yourinnercritic/src/components/ClosingComponent.tsx
@@ -1,13 +1,19 @@
 interface IClosingComponentProps {
   startOver: () => void;
+  closingRef: React.RefObject<HTMLElement>;
 }
 
-export const ClosingComponent = ({ startOver }: IClosingComponentProps) => {
+export const ClosingComponent = ({
+  startOver,
+  closingRef,
+}: IClosingComponentProps) => {
   return (
     <>
-      <h2>Great job!</h2>
-      <h3>Wanna do this all over with another situation?</h3>
-      <button onClick={startOver}>Start over</button>
+      <section ref={closingRef}>
+        <h2>Great job!</h2>
+        <h3>Wanna do this all over with another situation?</h3>
+        <button onClick={startOver}>Start over</button>
+      </section>
     </>
   );
 };
