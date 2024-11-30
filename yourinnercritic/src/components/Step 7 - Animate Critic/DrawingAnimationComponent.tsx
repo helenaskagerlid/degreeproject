5df433a5f1c653140502233ed6a5e1918eb63573
helenaskagerlid <helenaskagerlid@gmail.com>
2024-11-30 18:52:06 +0100

    :lipstick: responsive design step 7

--- yourinnercritic/src/components/Step 7 - Animate Critic/DrawingAnimationComponent.tsx
+++ yourinnercritic/src/components/Step 7 - Animate Critic/DrawingAnimationComponent.tsx
@@ -3,6 +3,7 @@ import { saveToLocalStorage } from "../../helpers/saveToLocalStorage";
 import { ScrollArrowComponent } from "../ArrowComponent/ArrowComponent";
 import { handleArrowClick } from "../../helpers/handleArrowClick";
 import { useVisibilityObserver } from "../../hooks/useVisibilityObserver";
+import "./stepSevenStyles.scss";
 
 interface IDrawingAnimationComponentProps {
   animationRef: React.RefObject<HTMLElement>;
@@ -38,41 +39,61 @@ export const DrawingAnimationComponent = ({
       <section className="animation-section" ref={animationRef}>
         <h2
           ref={headingRef}
-          className={`reveal-text ${isHeadingVisible ? "is-visible" : ""}`}
+          className={`step-seven-heading reveal-text ${
+            isHeadingVisible ? "is-visible" : ""
+          }`}
         >
           Step 7: Animate your inner critic
         </h2>
         <iframe
-          className="animation-image"
+          className="step-seven-animation-frame animation-image"
           src="https://sketch.metademolab.com/canvas"
           width="1100px"
           height="700px"
           loading="lazy"
           onLoad={(e) => e.currentTarget.classList.add("is-visible")}
         ></iframe>
-        <h3>
+        <p className="step-seven-text">
           Hey, before you go to the next step, remember to save the link for
           your favourite animation and put it below!
-        </h3>
-        <form onSubmit={handleSubmit}>
+        </p>
+        <img
+          className="step-seven-save-animation-img"
+          src="/share-animation-guide.png"
+          alt=""
+        />
+        <p>
+          Under your animated critic there should be a share button. Press that.
+        </p>
+        <img
+          className="step-seven-save-animation-img2"
+          src="/share-animation-guide3.png"
+          alt=""
+        />
+        <p>
+          Now click the little link icon and you have copied your
+          animation-link. Put that below.
+        </p>
+        <form className="step-seven-form" onSubmit={handleSubmit}>
           <input
+            className="step-seven-input"
             type="text"
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
           />
-          <button className="btn">Save</button>
+          <button className="step-seven-button btn">Save</button>
           {saved && <p>The animation was saved successfully</p>}
           {notSaved && <p>You have to add a link to your animation</p>}
         </form>
-        <button
-          onClick={() => {
-            handleArrowClick(setStepEight);
-          }}
-        >
-          {" "}
-          <ScrollArrowComponent />
-        </button>
       </section>
+      <div
+        onClick={() => {
+          handleArrowClick(setStepEight);
+        }}
+      >
+        {" "}
+        <ScrollArrowComponent />
+      </div>
     </>
   );
 };
